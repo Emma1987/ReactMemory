9bd5d99063399accc9243a17e0f3fd1a9020d57e
Emma1987 <mercadal.em@gmail.com>
2018-08-18 13:27:31 +0200

    3.5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import React, { Component } from 'react'
+import shuffle from 'lodash.shuffle'
+
 import './App.css'
+
 import Card from './Card'
 import GuessCount from './GuessCount'
+import HallOfFame, { FAKE_HOF } from './HallOfFame'
+
+const SIDE = 6
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
 
 class App extends Component {
-    handleCardClick (card) {
-        console.log(card, 'clicked')
+    cards = this.generateCards()
+
+    generateCards() {
+        const result = []
+        const size = SIDE * SIDE
+        const candidates = shuffle(SYMBOLS)
+        while (result.length < size) {
+            const card = candidates.pop()
+            result.push(card, card)
+        }
+        return shuffle(result)
+    }
+
+    // Arrow fx for binding
+    handleCardClick = (card) => {
+        console.log(card, this)
     }
 
     render() {
+        const won = new Date().getSeconds() % 2 === 0
         return (
             <div className="memory">
                 <GuessCount guesses={0} />
-                <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
-                <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
-                <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
-                <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
-                <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
-                <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+                {this.cards.map((card, index) => (
+                    <Card
+                        card={card}
+                        feedback='visible'
+                        key={index}
+                        onClick={this.handleCardClick}
+                    />
+                ))}
+                {won && <HallOfFame entries={FAKE_HOF} />}
             </div>
         )
     }
